1e8f0198257431c636fefbd8f2f76d59ca86f733
Rpinto30 <106196765+Rpinto30@users.noreply.github.com>
2026-04-16 01:40:40 -1200

    Mi idea 2, pero es muy costosa

--- 2021/problemA/solve.py
+++ 2021/problemA/solve.py
@@ -1,22 +1,23 @@
-grid = []
+grid = ""
 dx, dy, k = list(map(int, input().split()))
+
+for i in range(int(dy)* int(dx)): 
+    grid += '.'
+    
 
 for l in range(int(k)):
     entry = list(map(int, input().split()))
     wx,wy,d = entry[:3]
     pairs = entry[3:]
     
-    for p in range(len(pairs)//2):
-        pass
+    for p in range(0, len(pairs)-1, 2):
+        index = dx * (pairs[p+1]-1) + pairs[p]-1 # Mi idea es que se recorra como si fuese un cuadrado, dx veces y + x
+        grid = grid[:index] + "#" + grid[index+1:]
 
-
-for i in range(int(dy)): 
-    l = []
-    for j in range(int(dx)):
-        l.append('.')
-    grid.append(l)
 
 # ------- PRINT GRID ------
-for line in grid:
-    [print(i, end="") for i in line]
-    print()
+for ran in range(1, len(grid)+1):
+    if (ran%dx == 0): 
+        print(grid[ran-1])
+        continue
+    print(grid[ran-1], end='')
